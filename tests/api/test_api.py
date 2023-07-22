b0ec8cad1ec31b2fe0f4173247911a4a83531f3c
Daria Kozak <daria.kozak@3plearning.com>
2023-07-22 19:03:06 +0930

    With fixtures

--- tests/api/test_api.py
+++ tests/api/test_api.py
@@ -1,5 +1,31 @@
-def test_check_math():
+'''def test_check_math():
     assert 7*7 == 49
     
 def test_check_math_78():
-    assert 7*8 == 56
+    assert 7*8 == 56'''
+
+import pytest
+
+class User:
+    
+    def __init__(self) -> None:
+        self.name = 'Sergii'
+        self.second_name = 'Butenko'
+@pytest.fixture
+def user():
+    yield User()
+          
+    user = User()
+
+
+
+def test_remove_name(user):
+    user.name = ''
+    assert user.name == ''
+    
+def test_name(user):
+    assert user.name == 'Sergii'
+    
+    
+def test_second_name(user):
+    assert user.second_name == 'Butenko'
