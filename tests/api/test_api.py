a82a878854330a5b318646c047f7879eebe249ab
Daria Kozak <daria.kozak@3plearning.com>
2023-07-23 14:48:30 +0930

    fixtures are better coded

--- tests/api/test_api.py
+++ tests/api/test_api.py
@@ -4,7 +4,7 @@
 def test_check_math_78():
     assert 7*8 == 56'''
 
-import pytest
+'''import pytest
 
 class User:
     
@@ -15,17 +15,19 @@ class User:
 def user():
     yield User()
           
-    user = User()
+    user = User()'''
 
+import pytest
 
-
+@pytest.mark.change
 def test_remove_name(user):
     user.name = ''
     assert user.name == ''
-    
+
+@pytest.mark.check   
 def test_name(user):
     assert user.name == 'Sergii'
     
-    
+@pytest.mark.check    
 def test_second_name(user):
     assert user.second_name == 'Butenko'
