af8453875be77876478307178d380209c728e8cd
Daria Kozak <daria.kozak@3plearning.com>
2023-07-30 21:55:29 +0930

    Mandatory tests on Database completed

--- tests/database/test_database.py
+++ tests/database/test_database.py
@@ -34,8 +34,35 @@ def test_product_qnt_update():
     assert water_qnt[0][0] == 25
     
 @pytest.mark.database
-def print_all():
-    db =Database()
-    db.select_all
+def test_product_insert():
+    db = Database()
+    db.insert_product(4, "печиво", "солодке", 30)
+    product_qnt = db.select_product_qnt_by_id(4)
+    print(product_qnt[0][0])
+    #assert product_qnt[0][0] == 30
+    
+@pytest.mark.database
+def test_product_delete():
+    db = Database()
+    db.insert_product(99, 'тестові', 'дані', 999)
+    db.delete_product_by_id(99)
+    qnt = db.select_product_qnt_by_id(99)
+    
+    assert len(qnt) == 0
+    
+@pytest.mark.database
+def test_detailed_orders():
+    db = Database()
+    orders = db.get_detailed_orders()
+    print("Замовлення" , orders)
+    #Check quantity of orders equals to 1
+    assert len(orders) == 1
+    
+    #Check structure of data
+    assert orders[0][0] == 1
+    assert orders[0][1] == 'Sergii'
+    assert orders[0][2] == 'солодка вода'
+    assert orders[0][3] == 'з цукром'
+    
        
 #pytest -m database -s
